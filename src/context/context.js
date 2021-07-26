b458a14046ee0cf9a24df5d9a0b45bc54d1fc901
Tetiana Pomohaieva <t.pomogaeva@gmail.com>
2021-07-26 21:30:19 +0300

    update project

---
+++ src/context/context.js
@@ -0,0 +1,28 @@
+import React, { useReducer, createContext } from 'react';
+
+import contextReducer from './contextReducer';
+
+const initialState = JSON.parse(localStorage.getItem('transactions')) || [];
+
+export const ExpenseTrackerContext = createContext(initialState);
+
+export const Provider = ({ children }) => {
+  const [transactions, dispatch] = useReducer(contextReducer, initialState);
+
+  // Action Creators
+  const deleteTransaction = id => dispatch({ type: 'DELETE_TRANSACTION', payload: id });
+  const addTransaction = transaction => dispatch({ type:'ADD_TRANSACTION', payload: transaction });
+
+  const balance = transactions.reduce((acc, curVal) => curVal.type === 'Expense' ? acc - curVal.amount : acc + curVal.amount, 0); 
+
+  return (
+    <ExpenseTrackerContext.Provider value={{ 
+      deleteTransaction, 
+      addTransaction,
+      transactions,
+      balance
+     }}>
+      {children}
+    </ExpenseTrackerContext.Provider>
+  )
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { SpeechProvider } from '@speechly/react-client';
+
+import { Provider } from './context/context';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <SpeechProvider appId='afc647e5-545d-488f-a4db-1471768e9350' language='en-US'>
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>, 
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
